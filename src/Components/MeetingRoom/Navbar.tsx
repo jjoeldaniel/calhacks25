518376013a3027a78a7ed3da2c1f929e2df0ac54
Joel Daniel Rico <100006388+jjoeldaniel@users.noreply.github.com>
2025-10-25 22:02:07 -0700

    add back arrow functionality

--- src/Components/MeetingRoom/Navbar.tsx
+++ src/Components/MeetingRoom/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { ArrowLeft, Users, User } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+  const nav = useNavigate();
+
   return (
     <div className='w-full h-[77px]'>
       <div className="border-b border-zinc-800 p-4">
         <div className="max-w-4xl mx-auto flex items-center justify-between">
           <div className="flex items-center gap-4">
-            <button
+            <button onClick={() => nav("/")}
               className="inline-flex cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm transition-all h-9 px-3 hover:bg-zinc-800 text-white"
             >
               <ArrowLeft className="w-5 h-5" />
